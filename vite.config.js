602000b779fe8dba6190a38f0670b6f0316f3f7e
crowdvaulttoken-crypto <crowdvaulttoken@gmail.com>
2025-10-24 17:18:48 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), jsconfigPaths(), svgr(),eslint(), tailwindcss(),],
-  base: "/",
+  base: "/front-end",
 })
